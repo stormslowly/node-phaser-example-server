354a62814c0268481d6a16feffd2d7928a13ec2c
stormslowly@gmail.com <stormslowly@gmail.com>
2015-12-04 15:00:02 +0800

    file tree finish

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -18,26 +17,27 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', routes);
-app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
 
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -46,9 +46,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
+'use strict'
+var fs = require('fs')
+var path = require('path')
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var config = require('../config.json')
+
+router.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Express'
+    });
 });
+
+var base = config.examples_root
+router.post('/dir', function (req, res) {
+    var p = req.body.dir
+
+    var dir = p
+
+    var r = '<ul class="jqueryFileTree" style="display: none;">';
+    try {
+        r = '<ul class="jqueryFileTree" style="display: none;">';
+        var files = fs.readdirSync(dir);
+        files.forEach(function (f) {
+            var ff = dir + f;
+            var stats = fs.statSync(ff)
+            if (stats.isDirectory()) {
+                r += '<li class="directory collapsed"><a href="#" rel="' + ff + '/">' + f + '</a></li>';
+            } else {
+                var e = f.split('.')[1];
+                r += '<li class="file ext_' + e + '"><a href="#" rel=' + ff + '>' + f + '</a></li>';
+            }
+        });
+        r += '</ul>';
+    } catch (e) {
+        console.error('logs', e);
+        r += 'Could not load directory: ' + dir;
+        r += '</ul>';
+    }
+    res.send(r)
+})
 
 module.exports = router;
